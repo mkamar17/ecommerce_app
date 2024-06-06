4ec501ce0fa716e8f4e8926d3b9767711316b34b
Maryam Kamar <mkamar2740@gmail.com>
2024-06-06 19:59:11 +0100

    new home activity + xml -- login takes you to home, added search + nav bars

---
+++ app/src/main/java/com/maryam/ecommerce/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.maryam.ecommerce;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
