9a2f0334c3e1325f8fca4164a0b4e2301fedfc90
Maryam Kamar <mkamar2740@gmail.com>
2024-06-09 12:24:38 +0100

    added new fragments

---
+++ app/src/main/java/com/maryam/ecommerce/BrowseFragment.java
@@ -0,0 +1,22 @@
+package com.maryam.ecommerce;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BrowseFragment extends Fragment {
+
+    public BrowseFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_browse, container, false);
+    }
+}
