61da28179bbce9685f50ed74f31fce943c22f3a0
Maryam Kamar <mkamar2740@gmail.com>
2024-06-09 11:54:53 +0100

    working on bottom nav bar a different way (created new menu resource file)

--- app/src/main/java/com/maryam/ecommerce/HomeActivity.java
+++ app/src/main/java/com/maryam/ecommerce/HomeActivity.java
@@ -2,16 +2,25 @@ package com.maryam.ecommerce;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ImageView homeIcon;
+    private ImageView browseIcon;
+    private ImageView heartIcon;
+    private ImageView cartIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
 
 
     }
